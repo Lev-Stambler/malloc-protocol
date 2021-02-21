0ccb9d981211d5a07e0d0a70fff67f399b8707bf
Sebastien La Duca <sladuca777@gmail.com>
2021-02-21 17:50:05 -0500

    fix start-contract

--- scripts/start-contract.sh
+++ scripts/start-contract.sh
@@ -3,10 +3,11 @@
 
 (cd program && cargo build-bpf)
 
-solana-test-validator || "Already running"
+$(solana-test-validator &>/dev/null &) || "Already running"
 
 echo "$PWD/program/target/deploy/spl_memo.so" 
 solana program deploy "$PWD/program/target/deploy/spl_memo.so" -u l > src/config/program_id.json
+sleep 2
 
 
 solana logs -u l
