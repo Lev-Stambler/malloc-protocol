1ba47f9dea1dde9e300076f374691a29cb4f7793
Lev Stambler <levstamb@gmail.com>
2021-02-27 23:37:18 -0500

    test

--- functional-tests/test-malloc.ts
+++ functional-tests/test-malloc.ts
@@ -116,14 +116,19 @@ async function sendGeneralInstruction(
       // );
       tx.add(inst);
     });
-    const txRet = await sendAndConfirmTransaction(connection, tx, [account, ...signers], {
-      skipPreflight: true,
-      commitment: "singleGossip",
-    });
+    const txRet = await sendAndConfirmTransaction(
+      connection,
+      tx,
+      [account, ...signers],
+      {
+        skipPreflight: true,
+        commitment: "singleGossip",
+      }
+    );
     const data_account_info = await connection.getAccountInfo(
       data_account.publicKey
     );
-    return txRet
+    return txRet;
   } catch (e) {
     console.error(e);
     throw e;
@@ -197,8 +202,13 @@ describe("Run a standard set of Malloc tests", async function () {
 
     const createWCallAssociatedAccountsInsts: TransactionInstruction[] = [];
     await malloc_class.refresh();
-    const associatedAccountSigners: Account[] = []
-    const tok = new Token(connection, WRAPPED_SOL_MINT, TOKEN_PROGRAM_ID, account);
+    const associatedAccountSigners: Account[] = [];
+    const tok = new Token(
+      connection,
+      WRAPPED_SOL_MINT,
+      TOKEN_PROGRAM_ID,
+      account
+    );
 
     const I_GET_THE_MONEY = createTokenAccount(
       createWCallAssociatedAccountsInsts,
@@ -208,9 +218,15 @@ describe("Run a standard set of Malloc tests", async function () {
       account.publicKey,
       associatedAccountSigners
     );
-    await sendGeneralInstruction(createWCallAssociatedAccountsInsts, associatedAccountSigners)
-    const iGetMoneyStartingInfo = await tok.getAccountInfo(I_GET_THE_MONEY)
-    console.log("I get money starts out with", iGetMoneyStartingInfo.amount.toString())
+    await sendGeneralInstruction(
+      createWCallAssociatedAccountsInsts,
+      associatedAccountSigners
+    );
+    const iGetMoneyStartingInfo = await tok.getAccountInfo(I_GET_THE_MONEY);
+    console.log(
+      "I get money starts out with",
+      iGetMoneyStartingInfo.amount.toString()
+    );
 
     await doGeneralInstrSingleton({
       RegisterCall: {
@@ -225,6 +241,22 @@ describe("Run a standard set of Malloc tests", async function () {
         },
       },
     });
+    // TODO: somehow how registering this WCall causes issues...
+    // await doGeneralInstrSingleton({
+    //   RegisterCall: {
+    //     call_name: "takes money from one account to another chained",
+    //     // dummy public key
+    //     wcall_enum: {
+    //       Chained: {
+    //         wcall: serializePubkey(SIMPLE_PASS_THROUGH_WCALL),
+    //         input: "WSol",
+    //         output: "WSol",
+    //         callback_basket: "Just a simp",
+    //         associated_accounts: [],
+    //       },
+    //     },
+    //   },
+    // });
     await doGeneralInstrSingleton({
       CreateBasket: {
         name: "Just a simp",
@@ -237,21 +269,33 @@ describe("Run a standard set of Malloc tests", async function () {
         input: "WSol",
       },
     });
+    await doGeneralInstrSingleton({
+      CreateBasket: {
+        name: "Just a chained",
+        calls: [
+          "takes money from one account to another chained"
+          // "takes money from one account to another",
+        ],
+        splits: [1000],
+        input: "WSol",
+      },
+    });
+
 
     malloc_class.setUserPubKeyAlt(account.publicKey);
 
     await malloc_class.refresh();
     const rent = await malloc_class.getEphemeralAccountRent();
     const insts: TransactionInstruction[] = [];
-    const basketNode = malloc_class.getCallGraph("Just a simp");
-    console.log("Basket node", basketNode);
     const amountInFundedWSol = 1 * 1000 * 1000 * 1000;
     const fundedWSolAccount = await fundWithWSol(amountInFundedWSol + 1000);
 
-    const fundedWSolInfo = await tok.getAccountInfo(fundedWSolAccount)
+    const fundedWSolInfo = await tok.getAccountInfo(fundedWSolAccount);
 
-    console.log("WSol in funded with sol:", fundedWSolInfo.amount.toNumber())
+    console.log("WSol in funded with sol:", fundedWSolInfo.amount.toNumber());
 
+    const basketNode = malloc_class.getCallGraph("Just a simp");
+    console.log("Basket node", basketNode);
     const accounts = malloc_class.invokeCallGraph(
       insts,
       basketNode,
@@ -268,19 +312,39 @@ describe("Run a standard set of Malloc tests", async function () {
     // await sendGeneralInstruction([insts[1]], [accounts[0]])
     // await sendGeneralInstruction([insts[2]], [accounts[0], accounts[2]])
     const txRet = await sendGeneralInstruction(insts, accounts);
-    console.log("ENACT BASKET DONE, ", txRet)
+    console.log("ENACT BASKET DONE, ", txRet);
 
     const data = (await getDataParsed()) as MallocState;
     console.log("baskets:", data.baskets);
     assert(data.wrapped_calls["takes money from one account to another"]);
     assert(data.supported_wrapped_call_inputs["WSol"]);
     assert(data.baskets["Just a simp"]);
-    const iGetMoneyInfo = await tok.getAccountInfo(I_GET_THE_MONEY)
-    const amountIncrease = iGetMoneyInfo.amount.sub(iGetMoneyStartingInfo.amount).toNumber();
-    console.log("I get money gets this may more coins", amountIncrease)
+    const iGetMoneyInfo = await tok.getAccountInfo(I_GET_THE_MONEY);
+    const amountIncrease = iGetMoneyInfo.amount
+      .sub(iGetMoneyStartingInfo.amount)
+      .toNumber();
+    console.log("I get money gets this may more coins", amountIncrease);
     // account for payments
-    assert(amountInFundedWSol === amountIncrease)
-    const fundedWSolInfoNew = await tok.getAccountInfo(fundedWSolAccount)
-    console.log("WSol is now funded with sol:", fundedWSolInfoNew.amount.toNumber())
+    assert(amountInFundedWSol === amountIncrease);
+    const fundedWSolInfoNew = await tok.getAccountInfo(fundedWSolAccount);
+    console.log(
+      "WSol is now funded with sol:",
+      fundedWSolInfoNew.amount.toNumber()
+    );
+
+
+    // TODO change to sep fn
+    const instsChained = []
+    const basketChainedNode = malloc_class.getCallGraph("Just a chained");
+    console.log("Basket node", basketNode);
+    const accountsChained = malloc_class.invokeCallGraph(
+      instsChained,
+      basketChainedNode,
+      // TODO: make seconded funded with sol account
+      I_GET_THE_MONEY,
+      await malloc_class.getEphemeralAccountRent(),
+      amountInFundedWSol
+    );
+    const txRetChained = await sendGeneralInstruction(instsChained, accountsChained);
   });
 });
